171bc480382ceb06098da2139aedb309b8051f14
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-10-07 10:07:20 +0530

    Data Fetching

--- data_fetching_11/app/dataFetching/serverComp/page.jsx
+++ data_fetching_11/app/dataFetching/serverComp/page.jsx
@@ -1,3 +1,7 @@
+import { CgProfile } from "react-icons/cg";
+import { CiStar } from "react-icons/ci";
+import { SlGraph } from "react-icons/sl";
+import { HiOutlineUsers } from "react-icons/hi";
 
 const serverDataFetching = async (props) => {
 
@@ -7,9 +11,9 @@ const serverDataFetching = async (props) => {
   const URl = `https://api.genderize.io/?name=${userName}`
   const response = await fetch(URl)
   const userData = await  response.json()
-  console.log(userData)
 
   const confidencePercentage = userData.probability * 100;
+  const isMale = userData.gender === "male"
 
   if(!userName){
     return(
@@ -24,12 +28,40 @@ const serverDataFetching = async (props) => {
 
   return(
     <div>
-      <h1>Server Data Fetching</h1>
-      <p>UserName: {userData.name}</p>
-      <p>Gender: {userData.gender}</p>
-      <p>Count: {userData.count}</p>
-      <p>Probability: {confidencePercentage}</p>
-
+      <div className="w-[100%] h-[100vh] bg-red-100 flex items-center justify-center">
+        <div className="w-[350px]  border rounded bg-white border-gray-200 shadow-sm relative px-2 py-5">
+          <div className="w[100%] flex flex-col items-center justify-center ">
+            <div className={`w-[100px] h-[100px] border border-gray-200 rounded-full shadow-sm mt-2 flex items-center justify-center ${isMale ? "bg-blue-300" : "bg-pink-300"}`}><CgProfile className="text-4xl text-gray-100"/></div>
+            <h1 className="text-2xl font-bold text-gray-700 mt-1">{userData.name}</h1>
+            <div className={`h-[35px] w-[100px] border border-gray-200 rounded-full mt-2  ${isMale ? "bg-blue-300" : "bg-pink-300"} flex flex-row items-center justify-center gap-1`}>
+              <div className={`h-3 w-3 border rounded-full ${isMale ? "bg-blue-600 border-blue-700" : "bg-pink-600 border-pink-700"}`}></div>
+              <p className={`${isMale ? "text-blue-700" : "text-pink-700"}`}>{userData.gender.charAt(0).toUpperCase() + userData.gender.slice(1)}</p>
+            </div>
+            <div className="w-full h-[50px] border border-gray-100 mt-3 bg-gray-100 rounded-xl px-2">
+              <div className="w-full flex flex-row justify-between ">
+                <div className="flex flex-row items-center gap-0.5">
+                <CiStar className="text-xl"/>
+                <p>Confidence</p>
+                </div>
+                <p>{confidencePercentage}</p>
+              </div>
+              <div className={`h-3 border border-gray-200 mt-1 rounded-full ${isMale ? "bg-linear-65 from-blue-300 to-blue-600" : "bg-linear-65 from-pink-300 to-pink-600"}`}
+              style={{width: `${confidencePercentage}%`}}></div>
+            </div>
+            <div className="w-full h-[50px] border border-gray-100 mt-3 bg-gray-50 rounded-xl px-2 flex flex-row items-center justify-between">
+              <div className="flex flex-row items-center gap-0.5">
+              <SlGraph className="text-xl"/>
+              <p>Data Sample Size</p>
+              </div>
+              <p className="font-bold">{userData.count}</p>
+            </div>
+            <div className="w-full h-[50px] border-yellow-300 border mt-3 bg-yellow-100 rounded-xl px-2 flex flex-row items-center gap-0.5">
+            <HiOutlineUsers className="text-[14px] font-bold"/>
+            <p className="text-yellow-700 font-bold text-[13px]">Moderate Accuracy</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
