47098533df73c04e8045f53eeb8efc7b172bed11
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2025-10-11 09:23:55 +0530

    Connect DB

--- connect_db2_14/src/app/(user)/rendering/dynamic/page.jsx
+++ connect_db2_14/src/app/(user)/rendering/dynamic/page.jsx
@@ -1,25 +1,30 @@
+import { dataBase } from "@/lib/dataBase"
 import { Doctor } from "@/model/Doctordetails.model"
+
 export const dynamic = "force-dynamic"
 
-const doctorData = []
-try {
-  doctorData = await Doctor.find({})
-  console.log("data fetching successful in dynamic page")
-} catch (error) {
-  console.error("data fetching failed in dynamic page", error)
-}
 const DynamicRendering = async () => {
-  return(
+  let doctorData = []
+  try {
+    await dataBase()
+    doctorData = await Doctor.find({})
+    console.log("data fetching successful in dynamic page")
+
+  } catch (error) {
+    console.error("data fetching failed in dynamic page", error)
+    return <div>Failed to fetch data</div>
+  }
+
+  return (
     <div>
       <h1>Dynamic Rendering Page</h1>
-      {
-        doctorData.map((item, index) => (
-          <div key={index}>
-            <p>{item.firstName}</p>
-          </div>
-        ))
-      }
+      {doctorData.map((item, index) => (
+        <div key={index}>
+          <p>{item.firstName}</p>
+        </div>
+      ))}
     </div>
   )
 }
+
 export default DynamicRendering
